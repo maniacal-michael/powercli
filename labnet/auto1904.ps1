97351866cc7678a6fd4f99787ac8b6025072aa45
maniacal-michael <58062483+maniacal-michael@users.noreply.github.com>
2020-08-02 20:01:00 -0700

    Update auto1904.ps1

--- labnet/auto1904.ps1
+++ labnet/auto1904.ps1
@@ -1,7 +1,4 @@
-#$Spec=Get-OSCustomizationSpec 'centosx86' | New-OSCustomizationSpec -Name 'temp01' -Type NonPersistent
-#Get-OSCustomizationNicMapping -Spec $Spec | Set-OSCustomizationNicMapping -IPmode UseStaticIP -IpAddress ‘10.0.0.10‘ -SubnetMask ‘255.255.255.0‘ -DefaultGateway ‘10.0.0.1‘
-#$Template = Get-Template -Name ‘CentOS6.3-Base‘
-# Ignore above command for CentOS automation#
+#Start of Script
 ####################################################################################################################################################
 #set the customization for OSCustmization first
 ##########################################
@@ -32,11 +29,4 @@ New-VM -Name $newvm -Template $template -ResourcePool $mycluster -OSCustomizatio
 try {Get-VM $newvm | Get-NetworkAdapter | Set-NetworkAdapter -Portgroup $portgroup -Confirm:$false -ErrorAction Stop}
 catch {break}
 #}
-####Customization of the guest os failed for some settings --> New-VM : 8/2/2020 1:04:53 PM    New-VM          The operation for the entity "Win10_1904" failed with the following message: "Customization of the guest
-#operating system 'windows9_64Guest' is not supported in this configuration. Microsoft Vista (TM) and Linux guests with Logical Volume Manager are
-#supported only for recent ESX host and VMware Tools versions. Refer to vCenter documentation for supported configurations."
-#At G:\OneDrive\OneDrive - Clark College\PowerCLI\Labnet\auto1904.ps1:30 char:1
-#+ New-VM -Name $newvm -Template $template -ResourcePool $mycluster -OSC ...
-#+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#    + CategoryInfo          : NotSpecified: (:) [New-VM], UncustomizableGuest
-#    + FullyQualifiedErrorId : Client20_TaskServiceImpl_CheckServerSideTaskUpdates_OperationFailed,VMware.VimAutomation.ViCore.Cmdlets.Commands.NewVM
+#End of Script
